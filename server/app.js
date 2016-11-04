1cd35bb5ae8de063598063776eadfc53151b190a
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-04 12:11:36 -0500

    Adds catch-all route to server for unexpected get requests via React Router

--- server/app.js
+++ server/app.js
@@ -12,6 +12,12 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../server/public/app.html'));
 })
 
+//Catch-all route - MUST BE AT END - to handle unexpected use cases in React Router
+//Will serve the app.html file if another file is not found on the server
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '/server', '/public', 'app.html'))
+})
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
