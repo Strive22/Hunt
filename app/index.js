446de5beee7e465f1f6d9e1d71cb3ee5728ecd08
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-04 12:10:42 -0500

    Adds skeleton to index.js for React Router

--- app/index.js
+++ app/index.js
@@ -1,3 +1,20 @@
 //Entry point for React
 
 const React = require('react');
+const ReactDOM = require('react-dom');
+
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+
+const App = require('./components/App');
+const UserHome = require('./components/UserHome');
+
+// This is just a basic structure and some guesses at placeholders:
+
+// ReactDOM.render((
+//   <Router history={browserHistory}>
+//     <Route path='/' component={App}>
+//       <Route path='/users/:userid' component={UserHome} />
+//       <Route path='/jobs' component={} />
+//     </Route>
+//   </Router>
+// ), document.getElementById('app'));
