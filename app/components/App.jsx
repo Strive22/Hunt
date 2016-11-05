424b3263fcbbe4711c2b8785b751614c8d3d218f
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-04 21:14:37 -0500

    added basic routing, named App component with .jsx extension, added bundle.js to app.html, and built tester App component to test functionality of webpack

---
+++ app/components/App.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+import { browserHistory } from 'react-router';
+
+class App extends React.component {
+  render() {
+    return (
+      <div>
+      It works!
+      </div>
+    )
+  } 
+}
