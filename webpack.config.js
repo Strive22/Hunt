442a03793a0ddc959b31a44544ff708cf75b82a6
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-04 20:38:43 -0500

    adds babelrc file and makes minor comment changes to webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const webpack = require('webpack');
+
+// settings for all environments
+const commonConfig = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: './server/public/'
+  },
+
+  resolve: {
+    extensions: ['.jsx', '.js', '']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+}
+
+// development settings 
+const devConfig = {
+  devtool: 'source-maps',
+  devServer: {
+    inline: true,
+    historyApiFallback: true,
+    contentBase: './server/public/'
+  }
+}
+
+// production settings
+const prodConfig = {
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      comments: false,
+      compress: {
+        warnings: false
+      },
+      mangle: {
+        except: ['$'],
+        screw_ie8: true,
+        keep_fnames: true
+      }
+    })
+  ]
+}
+
+const config = {};
+
+// if TARGET is 'build' -> production mode
+// if TARGET is 'dev' -> development mode
+const TARGET = process.env.npm_lifecycle_event;
+switch (TARGET) {
+  case 'dev' :
+    Object.assign(config, commonConfig, devConfig);
+    break;
+  default : 
+    Object.assign(config, commonConfig, prodConfig);
+} 
+
+module.exports = config;
+
