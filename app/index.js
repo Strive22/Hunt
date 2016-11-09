089e0fafdbc4096edb8b3d4e3aaf49aceb7f7fe8
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-09 16:17:27 -0600

    Fixes wildcard route

--- app/index.js
+++ app/index.js
@@ -3,15 +3,18 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+// import { createStore } from 'redux';
 
 const App = require('./components/App');
 // const UserHome = require('./components/UserHome');
 
+// let store = createStore();
+
 // This is just a basic structure:
 
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-    </Route>
-  </Router>
+    <Router history={browserHistory}>
+      <Route path='/' component={App} />
+      <Route path='*' component={App} />
+    </Router>
 ), document.getElementById('app'));
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 //Catch-all route - MUST BE AT END - to handle unexpected use cases in React Router
 //Will serve the index.html file if another file is not found on the server
 router.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/public', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../public', 'index.html'))
 });
 
 module.exports = router;
