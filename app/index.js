29e2f1c49d7c0a7fef5ca394b775f310e0c1c219
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-09 17:53:32 -0600

    begins redux setup and filestructure

--- app/index.js
+++ app/index.js
@@ -5,11 +5,17 @@ const ReactDOM = require('react-dom');
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer.js';
+import changeTestResult from './actions/testActions.js';
 
 const App = require('./components/App');
 // const UserHome = require('./components/UserHome');
 
-// let store = createStore();
+let store = createStore(rootReducer);
+
+console.log('store.getState result:', store.getState());
+
+store.dispatch(changeTestResult('the test passed!'));
 
 // This is just a basic structure:
 
