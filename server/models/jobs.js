82d4a49ae2439ff8a07c12634d41ac763152e298
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-10 14:02:40 -0600

    removes _id property from user and jobs models, not necessary

--- server/models/jobs.js
+++ server/models/jobs.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const jobsSchema = new Schema({
-  // _id: String,
   api: String,
   apiSpecificId: String,
   title: String,
