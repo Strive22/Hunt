6eb941d06ac6be25444d449a2e2f69004204e8bb
Tulasi Bandi <tulasibandi@gmail.com>
2016-11-09 19:54:23 -0600

    Modifed Routes

--- app/index.js
+++ app/index.js
@@ -6,6 +6,10 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 // import { createStore } from 'redux';
 
 const App = require('./components/App');
+const Land = require('./components/Landingpage');
+const Hunt = require('./components/Hunt');
+const Dash = require('./components/ViewDashBoard');
+const search = require('./components/SearchForJob');
 // const UserHome = require('./components/UserHome');
 
 // let store = createStore();
@@ -14,7 +18,11 @@ const App = require('./components/App');
 
 ReactDOM.render((
     <Router history={browserHistory}>
-      <Route path='/' component={App} />
-      <Route path='*' component={App} />
+      <Route path='/' component={Hunt}>
+      <IndexRoute component ={Land}/>
+      <Route path ='Land' component ={Land}/>
+      <Route path ='Dash' component ={Dash}/>
+      <Route path ='search' component ={search}/>
+      </Route>
     </Router>
 ), document.getElementById('app'));
