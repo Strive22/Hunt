50c18fd93e1ccb31b18a15f7705028e31f431950
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-10 10:21:09 -0600

    adds request module

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const request = require('request');
 const model = require('./models/search_models.js')
 
 //search the API of the user's choice for jobs
 
 //github
-router.get('/gh/:search', model.hubHunt)
+router.get('/gh/:searchterms', model.hubHunt)
 
 //authentic jobs
-router.get('/aj/:search', model.authenticHunt)
+router.get('/aj/:searchterms', (req, res) => {
+  //search terms must be comma-separated - need this to be clear to the user
+  let searchTerms = req.params.searchterms;
+  //location will be a query string ?loc=
+  let location = req.query.loc;
+  let options = {
+    url: `https://authenticjobs.com/api/?api_key=${process.env.AJ_KEY}&method=aj.jobs.search&loc=${location}&keywords=${searchTerms}`
+  }
+})
 
 //indeed
-router.get('/in/:search', model.indeedHunt)
+router.get('/in/:searchterms', model.indeedHunt)
 
 
 
