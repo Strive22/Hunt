fa3965a98bdef7295b165cfcce3bef0c27b3a2b6
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-11 16:14:29 -0600

    temporarily removes event handlers on most form fields

--- app/components/views/EnterJob.js
+++ app/components/views/EnterJob.js
@@ -1,13 +1,86 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Modal, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
-export default () => {
+class EnterJob extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      value: ''
+    }
+  }
 
-  return (
-    <div className="selectbox">
-      <h2>Enter<br/>a Job</h2>
-      <Button bsClass="btn">Go!</Button>
-    </div>
-  );
+  openModal() {
+    this.setState({
+      showModal: true,
+    })
+  }
 
+  closeModal() {
+    this.setState({
+      showModal: false,
+    })
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="selectbox">
+        <h2>Enter<br/>a Job</h2>
+        <Button bsClass="btn" onClick={this.openModal.bind(this)}>Go!</Button>
+
+        <Modal
+          aria-labelledby='modal-label'
+          show={this.state.showModal}
+          onHide={this.closeModal.bind(this)}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Describe the Job.</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FormGroup>
+              <FormControl
+                type="text"
+                value={this.state.value}
+                placeholder="Job Title"
+                onChange={this.handleChange.bind(this)}
+              />
+              <br/>
+              <FormControl
+                type="text"
+                value={this.state.value}
+                placeholder="Company"
+              />
+              <br/>
+              <FormControl
+                type="text"
+                value={this.state.value}
+                placeholder="Location"
+              />
+              <br/>
+              <FormControl
+                type="text"
+                value={this.state.value}
+                placeholder="Link to Job Posting"
+              />
+              <br/>
+              <FormControl
+                type="textarea"
+                value={this.state.value}
+                placeholder="Description"
+              />
+            </FormGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button>Submit</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 };
+
+module.exports = EnterJob
