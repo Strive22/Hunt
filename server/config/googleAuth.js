2d93880779f2ce9ab1d7b7ae49e1e3fce40b9132
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-12 15:53:38 -0600

    requires passport, begins building passport auth routes, adds /landing route to index.js for failed auth

--- server/config/googleAuth.js
+++ server/config/googleAuth.js
@@ -1,3 +1,19 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.HOST
+}))
+
+
+
+
+
+
+
+
 exports.onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
