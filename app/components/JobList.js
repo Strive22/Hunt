97a6d01b71fb8835196df4ab8eea6e85c495e917
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-16 13:00:01 -0600

    builds three JobList scenarios for rendering interested, in progress, or completed jobs

--- app/components/JobList.js
+++ app/components/JobList.js
@@ -2,17 +2,68 @@ import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import JobListItem from './JobListItem'
 
-export default (props) => {
+const JobList = (props) => {
+  let interested, inProgress, complete;
 
-  props.newSearchResults.map((searchResult, index) => (
+  //if we're rendering the interested list
+  if (props.interested) {
+    interested = props.interested.map(job => {
+      return (
+        <JobListItem 
+          jobContent={props.jobContent}
+          addJobToInProgress={props.addJobToInProgress}
+          addJobToComplete={props.addJobToComplete}
+          removeJob={props.removeJob}
+        />
+      )
+    })
+    return (
+      <div className="job-list-box">
+        <ListGroup>
+          {interested}
+        </ListGroup>
+      </div>
+    )
+  }
 
-  )
+  //if we're rendering the inProgress list
+  if (props.inProgress) {
+    inProgress = props.inProgress.map(job => {
+      return (
+        <JobListItem 
+          jobContent={props.jobContent}
+          addJobToComplete={props.addJobToComplete}
+          removeJob={props.removeJob}
+        />
+      )
+    })
+    return (
+      <div className="job-list-box">
+        <ListGroup>
+          {inProgress}
+        </ListGroup>
+      </div>
+    )
+  }
 
-  return (
-    <div className="job-list-box">
-      <ListGroup>
-        {}
-      </ListGroup>
-    </div>
-  )
+  //if we're rendering the complete list
+  if (props.complete) {
+    complete = props.complete.map(job => {
+      return (
+        <JobListItem
+          jobContent={props.jobContent}
+          removeJob={props.removeJob}
+        />
+      )
+    })
+    return (
+      <div className="job-list-box">
+        <ListGroup>
+          {complete}
+        </ListGroup>
+      </div>
+    )
+  }
 }
+
+module.exports = JobList;
