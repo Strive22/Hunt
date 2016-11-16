2e5df0d7eb06daa9ec3a245ad6efbbec255e67ba
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-16 12:46:55 -0600

    passes props to job lists for each tab

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { browserHistory, Link } from 'react-router';
+import JobList from './JobList';
+import JobListItem from './JobListItem';
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -8,15 +10,44 @@ class Dashboard extends React.Component {
       interested: this.props.interested,
       inProgress: this.props.inProgress,
       complete: this.props.complete,
-      jobContent: this.props.jobContent
+      jobContent: this.props.jobContent,
+      key: 1
     }
+  }
+
+  handleSelect(key) {
+    this.setState({ key });
   }
 
   render() {
     return (
-      <div>
-        <h1>Dashboard</h1>
-      </div>
+      <Tabs activeKey={this.state.key} onSelect={this.handleSelect} class="dashtabs">
+        <Tab eventKey={1} title="Jobs I'm Interested In">
+          <JobList 
+            jobContent={this.state.jobContent}
+            interested={this.state.interested}
+            addJobToInProgress={this.props.addJobToInProgress} 
+            addJobToComplete={this.props.addJobToComplete} 
+            removeJob={this.props.removeJob}
+          />
+        </Tab>
+        <Tab eventKey={2} title="Job Applications In Progress">
+          <JobList 
+            jobContent={this.state.jobContent}
+            inProgress={this.state.inProgress}
+            addJobToComplete={this.props.addJobToComplete} 
+            removeJob={this.props.removeJob}            
+          />
+
+        </Tab>
+        <Tab eventKey={3} title="Job Applications Complete">
+          <JobList 
+            jobContent={this.state.jobContent}
+            complete={this.state.complete}
+            removeJob={this.props.removeJob}            
+          />
+        </Tab>
+      </Tabs>
     )
   }
   
