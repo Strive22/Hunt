94ce58c5f818b28f5a9ce239599720d637b9270d
Scott <scotthorn0@gmail.com>
2016-11-22 11:25:18 -0600

    Updated NewSearchResults to properly render

--- app/components/NewSearchResults.js
+++ app/components/NewSearchResults.js
@@ -9,14 +9,12 @@ class NewSearchResults extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      results: this.props.searchResults,
       spinner: true
     }
   }
 
   componentWillReceiveProps(nextProps) {
     this.setState({
-      results: nextProps.searchResults,
       spinner: false
     })
   }
@@ -35,7 +33,7 @@ class NewSearchResults extends React.Component {
         <Row>
           <Col md={12}>
             {/* {spinner} */}
-            <SearchResultsList jobs={this.state.results} addJob={this.props.addJob}/>
+            <SearchResultsList jobs={this.props.searchResults} addJob={this.props.addJob}/>
           </Col>
         </Row>
 
