a0a379e511ba90db760fc8aeb809c1abc99dffce
Brittany Artimez <brittanyartimez@gmail.com>
2016-11-22 22:55:19 -0600

    adds some popovers to job content

--- app/components/JobContent.js
+++ app/components/JobContent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Form, FormControl, FormGroup, ControlLabel, Grid, Col, Row, HelpBlock, Popover, OverlayTrigger } from 'react-bootstrap';
+import picker from '../picker';
 
 class JobContent extends React.Component {
   constructor(props) {
@@ -11,7 +12,12 @@ class JobContent extends React.Component {
 
   render () {
     const notesPop = (
-      <Popover id="popover-positioned-right" title="Add some notes!"> Use this area as a place to store your own notes about a job you're interested in or working on.  We'll save them and display them here for you every time you open this job.
+      <Popover id="notes-pop" title="Add some notes!">Use this area as a place to store your own notes about a job you are interested in or working on.  We will save them and display them here for you every time you open this job.
+      </Popover>
+    );
+
+    const pickerPop = (
+      <Popover id="picker-pop" title="Grab your resume!">Press this button to grab documents you have already saved in Google Docs and associate them with this job.
       </Popover>
     );
 
@@ -33,7 +39,6 @@ class JobContent extends React.Component {
           <Button className="modal-btns">Save This Note</Button>
         </Form>
         <hr/>
-        Some dummy text where other things will go
       </div>
     )
   }
